0106177225f5e208f0174b8686ac062578b5fec6
Farzaneh-ghasemi <ghasemik.farzaneh@gmail.com>
2025-06-04 13:33:38 +0200

    change db connection

--- configs/db.js
+++ configs/db.js
@@ -2,16 +2,10 @@ const mongoose = require("mongoose");
 
 const connectToDB = async () => {
   try {
-    console.log("Connecting to:", process.env.MONGO_URL);
-
     if (mongoose.connections[0].readyState) {
-      // Already connected
       return true;
     } else {
-      await mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+      await mongoose.connect(process.env.MONGO_URL);
       console.log("Connected to DB successfully");
     }
   } catch (err) {
